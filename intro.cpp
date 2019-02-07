ec931f21602246525df679a486f9d3adf78d56c4
FlyinDutchmanV <FlyinDutchmanV@users.noreply.github.com>
2019-02-07 14:41:07 -0400

    Added source file

---
+++ intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age(0); //an alternative way to declare and initialise age.
+	cout << "Hello. How old are you?" << endl;
+	cin >> age;
+	cout << "Good luck buying " << age << "candles!" << endl;
+	return 0;
+}
